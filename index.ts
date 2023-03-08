eb000e8d13bc080791b490cd0cb9eec4c91b61bf
Yating Song <yatingsong7@gmail.com>
2023-03-08 14:19:45 +1100

    set up database connection

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { DataSource } from "typeorm";
 
 //Instrantiate express app
 const app: Express = express();
@@ -7,15 +8,33 @@ const app: Express = express();
 //Get environment variables
 dotenv.config();
 
+// Create Database Connection
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "localhost",
+  port: 3307,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true, //Since it's a small project, it's ok to set it to true
+  entities: [],
+});
+
 // define server port
 const port = process.env.PORT;
 
 // Create a default route
 app.get("/", (req: Request, res: Response) => {
-  res.send("Server starts on 3200");
+  res.send("Server starts on 3200 and DB starts on 3307");
 });
 
-// Start listening to the requests on the default port
-app.listen(port, () => {
-  console.log("Server is on Port 3200");
-});
+// Once DB is connected, start listening to the requests on the default port
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Server is on Port 3200");
+    });
+  })
+  .catch((err) => {
+    console.error("Error of initialization: ", err);
+  });
